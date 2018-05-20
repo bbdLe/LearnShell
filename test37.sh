924cb7b4e92a8ce1e2be677c9a1b98c8760e5e41
bbdLe <dalibaxiaoliba19@gmail.com>
2018-05-20 09:45:03 +0800

    another example of how not to use the for command

---
+++ test37.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# another example of how not to use the for command
+#
+
+for test in I don't know if this'll work
+do
+    echo "word:$test"
+done
+
+echo
+echo
+
+for test in I "don't" know if "this'll" work
+do
+    echo "word:$test"
+done
